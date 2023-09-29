905ad55e1d29af4a3a85a0e92d0fba0e4c3d49ef
Juan Alegría <juanszalegria@gmail.com>
2023-09-29 11:12:42 -0500

    Feat: add product detail component

---
+++ frontend/src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../shared/product.service';
+import { Product } from '../shared/product.model';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css'],
+})
+export class ProductDetailComponent implements OnInit {
+  product: Product | undefined;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const productId = params['id'];
+      if (productId) {
+        this.productService.getProductById(productId).subscribe((data) => {
+          this.product = data;
+        });
+      }
+    });
+  }
+
+  // Handle editing a product
+  editProduct() {
+    if (this.product && this.product._id) {
+      this.router.navigate(['/edit-product', this.product._id]);
+    }
+  }
+
+  // Handle deleting a product
+  deleteProduct() {
+    if (this.product && this.product._id) {
+      const confirmDelete = window.confirm(`Are you sure you want to delete ${this.product.name}?`);
+      if (confirmDelete) {
+        this.productService.deleteProduct(this.product._id).subscribe(() => {
+          // Product deleted, navigate back to the product list.
+          this.router.navigate(['/product-list']);
+        });
+      }
+    }
+  }
+}
