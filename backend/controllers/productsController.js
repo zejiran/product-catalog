190098f54f96b248d8714967a9465662f10b92a6
Juan Alegría <juanszalegria@gmail.com>
2023-09-29 10:27:32 -0500

    Feat: add get product by id endpoint

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -20,6 +20,16 @@ const getAllProducts = asyncHandler(async (req, res) => {
     res.status(200).json(products)
 });
 
+const getProductById = asyncHandler(async (req, res) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if (!product) {
+        res.status(404);
+        throw new Error('Product not found');
+    }
+    res.status(200).json(product);
+});
+
 const updateProduct = asyncHandler(async (req, res) => {
     const productId = req.params.id;
 
@@ -62,6 +72,7 @@ const deleteProduct = asyncHandler(async (req, res) => {
 module.exports = {
     createProduct,
     getAllProducts,
+    getProductById,
     updateProduct,
     deleteProduct
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct, getAllProducts, updateProduct, deleteProduct } = require('../controllers/productsController');
+const { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } = require('../controllers/productsController');
 
 router.route('/')
     .get(getAllProducts)
     .post(createProduct);
 
 router.route('/:id')
+    .get(getProductById)
     .put(updateProduct)
     .delete(deleteProduct);
 
