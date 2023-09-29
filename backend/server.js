94e67e44b80892956b3774d9b28f0e579b712378
Juan Alegría <juanszalegria@gmail.com>
2023-09-29 09:04:01 -0500

    Feat: create error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const port = 3001;
 const connectDB = require('./config/db');
+const { errorHandler } = require('./middleware/errorMiddleware');
 require('dotenv').config();
 
+const port = 3001;
+
+// Connect to the database
 connectDB();
+
+// Create an Express app
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+// Middleware
+app.use(errorHandler);
 
-app.use('/api/products', require('./routes/productRoutes'))
+// Define API routes
+app.use('/api/products', require('./routes/productRoutes'));
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
