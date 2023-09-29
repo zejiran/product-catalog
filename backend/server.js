00cfff8ef70ea1b26ab1654856ecb145b3f2131b
Juan Alegría <juanszalegria@gmail.com>
2023-09-29 11:29:13 -0500

    Fix: allow cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
 require('dotenv').config();
@@ -10,6 +11,7 @@ connectDB();
 
 // Create an Express app
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
