692fa5c7d985020d61c0dcb1dd84377fb4342fb1
Juan Alegría <juanszalegria@gmail.com>
2023-09-29 10:30:27 -0500

    Feat: add product model and service

---
+++ frontend/src/app/shared/product.model.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    name: string;
+    description: string;
+    sku: string;
+    image: string;
+    tags: string[];
+    price: number;
+    stock: number;
+}
---
+++ frontend/src/app/shared/product.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Product } from './product.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ProductService {
+    private apiUrl = 'http://localhost:3001/api/products';
+
+    constructor(private http: HttpClient) { }
+
+    // Get all products
+    getAllProducts(): Observable<Product[]> {
+        return this.http.get<Product[]>(this.apiUrl);
+    }
+
+    // Get a product by ID
+    getProductById(id: string): Observable<Product> {
+        const url = `${this.apiUrl}/${id}`;
+        return this.http.get<Product>(url);
+    }
+
+    // Create a new product
+    createProduct(product: Product): Observable<Product> {
+        return this.http.post<Product>(this.apiUrl, product);
+    }
+
+    // Update a product by ID
+    updateProduct(id: string, product: Product): Observable<Product> {
+        const url = `${this.apiUrl}/${id}`;
+        return this.http.put<Product>(url, product);
+    }
+
+    // Delete a product by ID
+    deleteProduct(id: string): Observable<{ message: string; id: string }> {
+        const url = `${this.apiUrl}/${id}`;
+        return this.http.delete<{ message: string; id: string }>(url);
+    }
+}
